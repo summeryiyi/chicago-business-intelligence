06cbee24360f4475aec1cc528879158aafad8b67
小giao兔 <85620262+summeryiyi@users.noreply.github.com>
2022-05-02 04:17:16 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -174,7 +174,7 @@ func main() {
 
 	//Option 4
 	//Database application running on Google Cloud Platform.
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/driven-park-348917:asia-east1:mypostgres sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/voltaic-talent-348919:asia-east1:mypostgres sslmode=disable port = 5432"
 
 	db, err := sql.Open("postgres", db_connection)
 	if err != nil {
